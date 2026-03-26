5fed5755ec72c6b7d0dea64439f3c7c9a8fc9f5a
UrosPetronijevic <ukipetlja@gmail.com>
2026-03-26 16:06:20 -0400

    added 0G_VUEFACTS folder and separated header main and footer into its own components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
-<template></template>
+/*
+CHALLENGE: Turn the provided HTML/CSS mockup into a Vue app that
+           uses three components: Header, Main, and Footer.
+</script>
+
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
 
 <style scoped></style>
